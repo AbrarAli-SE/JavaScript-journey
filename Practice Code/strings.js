fd7138045eaacd18cff08ee2847e6e8f916c81e1
AbrarAli-SE <abrarali.se@gmail.com>
2025-03-15 01:03:08 +0500

    Create strings.js

---
+++ Practice Code/strings.js
@@ -0,0 +1,36 @@
+const name = "Ali"
+const age = 50
+
+// console.log(name + repoCount + " Value");
+// console.log(`my name is ${name}, and my age is ${age}` );
+
+
+
+const gameName = new String('hitesh-hc-com')
+
+// console.log(gameName[10]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('h'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(2, 4)
+// console.log(anotherString);
+
+// const newStringOne = "   hitesh    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim()); //Removes the leading and trailing white space and line terminator characters from a string.
+
+// const url = "https://hitesh.com/hitesh%20choudhary"
+
+// console.log(url.replace('%20', '-'))
+
+// console.log(url.includes('sundar'))
+
+// console.log(gameName.split('-'));
